b0e4692d4b775a7baaa494b46a2226b54a352a9e
Vinay Shetye <vinay.courses.official@gmail.com>
2025-01-31 20:36:43 -0500

    updated file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App'
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap/dist/js/bootstrap.js'
+/**
+ * Entry point of the React Todo application.
+ *
+ * This file is responsible for:
+ * - Mounting the root React component to the DOM
+ * - Importing global styles and dependencies
+ * - Setting up the initial render
+ *
+ * Dependencies:
+ * - React: Core library for building UI components
+ * - ReactDOM: Library for DOM-specific methods
+ * - Bootstrap: CSS framework for styling
+ * - index.css: Global application styles
+ *
+ * @module index
+ */
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
