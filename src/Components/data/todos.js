89b527006762ee229f7cb879f7e80123fff04b8d
Vinay Shetye <vinay.courses.official@gmail.com>
2025-01-31 20:27:22 -0500

    updated file

---
+++ src/Components/data/todos.js
@@ -0,0 +1,29 @@
+const todos = [
+  {
+    id: 1,
+    todo: "Complete Database Project Report",
+    checked: false,
+  },
+  {
+    id: 2,
+    todo: "Prepare Funding Request for Cyber Security Club",
+    checked: false,
+  },
+  {
+    id: 3,
+    todo: "Review Data Mining Project Audio Extraction",
+    checked: true,
+  },
+  {
+    id: 4,
+    todo: "Optimize SQLite Queries",
+    checked: false,
+  },
+  {
+    id: 5,
+    todo: "Submit AI Course Project 3 Proposal",
+    checked: false,
+  },
+];
+
+export default todos;
