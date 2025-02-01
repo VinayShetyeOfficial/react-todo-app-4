dad0ded0c91c6bd0733ca4941644cd850b18d6a7
Vinay Shetye <vinay.courses.official@gmail.com>
2025-01-31 20:36:32 -0500

    updated file

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,21 +1,43 @@
-import React from 'react'
-import './Home.css'
-import Button from 'react-bootstrap/Button';
-import { useNavigate } from 'react-router-dom';
+/**
+ * Home component serves as the landing page for the Todo application.
+ *
+ * Features:
+ * - Displays welcome message
+ * - Provides navigation to the main todo interface
+ * - Implements animated button text
+ *
+ * Props: None
+ *
+ * Hooks used:
+ * - useNavigate: For programmatic navigation to the main todo interface
+ *
+ * Styling:
+ * - Uses custom CSS from Home.css
+ * - Implements Bootstrap Button component
+ *
+ * @component
+ * @returns {JSX.Element} Rendered Home component
+ */
+import React from "react";
+import "./Home.css";
+import Button from "react-bootstrap/Button";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
-
-  const navigate = useNavigate()
+  // Initialize navigation hook
+  const navigate = useNavigate();
 
   return (
-    <div className='main_Home'>
-      <div className='container_Home'>
-        <h1 className='heading_Home'>Welcome to our Todo list App</h1>
+    <div className="main_Home">
+      <div className="container_Home">
+        <h1 className="heading_Home">Welcome to our Todo list App</h1>
+        {/* Navigation button with animated text */}
         <Button
-          className='btn_Home btn1'
+          className="btn_Home btn1"
           variant="outline-primary"
-          onClick={() => navigate('/main')}>
-          <span class="back">
+          onClick={() => navigate("/main")}
+        >
+          <span className="back">
             <span>G</span>
             <span>e</span>
             <span>t</span>
@@ -31,7 +53,7 @@ function Home() {
         </Button>
       </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
