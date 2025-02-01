a39d763a41f9f82906a3de4b799db4125896c9a6
Vinay Shetye <vinay.courses.official@gmail.com>
2025-01-31 20:36:23 -0500

    updated file

--- src/App.js
+++ src/App.js
@@ -1,3 +1,14 @@
+/**
+ * Root component of the Todo application.
+ * Handles routing between Home and Main components.
+ *
+ * Routes:
+ * - /: Home component (welcome page)
+ * - /main: Main component (todo list interface)
+ *
+ * @component
+ * @returns {JSX.Element} The rendered App component
+ */
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
