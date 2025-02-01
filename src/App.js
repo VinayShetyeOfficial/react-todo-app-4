1269bab0d76dce340c94086e27a2e612f1bd42b6
Vinay Shetye <vinay.courses.official@gmail.com>
2025-01-31 20:27:16 -0500

    updated file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import './App.css';
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './Components/Home/Home';
-import Main from './Components/Main/Main';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Main from "./components/Main/Main";
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route path='/main' element={<Main />} />
+          <Route exact path="/" element={<Home />} />
+          <Route path="/main" element={<Main />} />
         </Routes>
       </Router>
     </>
